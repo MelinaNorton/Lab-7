c9422950c2f2cc5b2841b8205845ed5c04edc8dd
MelinaNorton <127869740+MelinaNorton@users.noreply.github.com>
2024-03-26 15:48:07 -0400

    Add files via upload

---
+++ Lab 7 2150 Decorator Pattern/IShuffleList.java
@@ -0,0 +1,16 @@
+package cpsc2150.listDec;
+import java.util.List;
+
+public interface IShuffleList<T> extends List<T> {
+    default void shuffle(int swaps) {
+
+    }
+
+    default void swap(int i, int j) {
+
+    }
+
+    public default void randomize(){
+
+    }
+}
